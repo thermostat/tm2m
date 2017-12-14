ee6a64db099f7ecda7478db79fbfc22c14dc424d
Dan Williams <dww@pyrento.net>
2017-12-14 15:01:40 -0600

    skeleton of history interface

---
+++ tm2m/named_histories.py
@@ -0,0 +1,69 @@
+
+
+"""
+Managed history lists
+"""
+
+import json
+
+def get_serializer():
+    return json
+
+def strn_trunc(strn, limit=80):
+    if len(strn) > limit:
+        return strn[:limit-3]+"..."
+    return strn
+
+class HistoryItem(object):
+
+    @staticmethod
+    def load(obj):
+        return HistoryItem(**obj)
+
+    def __init__(self, cmd, shortname=None):
+        self.cmd = cmd
+        self.shortname = shortname
+
+    def __eq__(self, other):
+        return self.cmd == other.cmd
+
+    def startswith(self, prefix):
+        return self.cmd.startswith(prefix)
+
+    def serialize(self):
+        return vars(self)
+
+    def __repr__(self):
+        return strn_trunc(self.cmd, 20)
+
+
+class History(object):
+    def __init__(self, name, fname=None):
+        self.name = name
+        self.lst = []
+        self.fname = fname
+
+    def add_item(self, cmd, shortname=None):
+        hi = HistoryItem(cmd, shortname)
+        self.lst.append(hi)
+
+    def search(self, prefix):
+        return [x for x in self.lst if x.startswith(prefix)]
+
+    def save(self):
+        if self.fname:
+            serial = get_serializer()
+            fd = open(self.fname, 'w')
+            serial.dump([x.serialize() for x in self.lst], fd)
+
+    def load(self):
+        if self.fname:
+            serial = get_serializer()
+            fd = open(self.fname, 'r')
+            self.lst = [ HistoryItem.load(x) for x in 
+                         serial.load(fd) ]
+
+class HistoryMap(object):
+    def __init__(self, history_save_dir=None):
+        self.save_dir = history_save_dir
+        
